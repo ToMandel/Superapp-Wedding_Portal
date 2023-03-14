c59a8ad9167a950fa5f8f0e95b6b5224982c8c61
dean6 <dean6@DESKTOP-GIS1O58>
2023-03-14 18:17:30 +0200

    Add GET all objects request

--- src/main/java/superapp/SuperAppAPI.java
+++ src/main/java/superapp/SuperAppAPI.java
@@ -15,6 +15,7 @@ import boundries.UserId;
 
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,7 +30,7 @@ public class SuperAppAPI {
 		path = {"/superapp/objects/{superapp}/{internalObjectId}"},
 		method = {RequestMethod.GET},
 		produces = {MediaType.APPLICATION_JSON_VALUE})
-	public CustomObject GetObject (@PathVariable("superapp")String superapp, @PathVariable("internalObjectId")String internalObjectId) {
+	public CustomObject GetObjectById (@PathVariable("superapp")String superapp, @PathVariable("internalObjectId")String internalObjectId) {
 		ObjectId objectId = new ObjectId(superapp, internalObjectId);
 		String type = "Customer";
 		String alias = "demo customer"; 
@@ -44,4 +45,30 @@ public class SuperAppAPI {
 		return customObject;
 	
 	}
+	
+	
+	@RequestMapping(
+			path = {"/superapp/objects"},
+			method = {RequestMethod.GET},
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+		public ArrayList<CustomObject> GetAllObjects () {
+			String superapp = "wedding";
+			ArrayList<CustomObject> list = new ArrayList<CustomObject>();
+			for (int i = 0; i < 3; i++) {
+				String internalObjectId = i + "";
+				ObjectId objectId = new ObjectId(superapp, internalObjectId);
+				String type = "Customer";
+				String alias = "customer" + i; 
+				Date creationTimestamp = new Date();
+				boolean active = true;
+				Location location = new Location(10.10, 11.11);
+				CreatedBy createdBy = new CreatedBy(new UserId(superapp, "e-mail@demo.com"));
+				Map<String, Object> objectDetails = new HashMap<String, Object>();
+				objectDetails.put("name", "liri");
+				objectDetails.put("phone", "0525511");
+				list.add(new CustomObject(objectId, type, alias, active, creationTimestamp, location, createdBy, objectDetails));
+			}
+			return list;
+		
+		}
 }
