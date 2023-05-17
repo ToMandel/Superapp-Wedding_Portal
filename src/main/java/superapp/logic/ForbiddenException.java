d891c2880a7c75cbce7b30c08ed2f0cfd564ae62
dean.avram <dean.avram@s.afeka.ac.il>
2023-05-17 20:37:51 +0300

    Added 403 and 401 exception classes

---
+++ src/main/java/superapp/logic/ForbiddenException.java
@@ -0,0 +1,25 @@
+package superapp.logic;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException{
+
+    private static final long serialVersionUID = 4988412318524322266L;
+
+    public ForbiddenException() {
+    }
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+    public ForbiddenException(Throwable cause) {
+        super(cause);
+    }
+
+    public ForbiddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/superapp/logic/UnauthorizedException.java
@@ -0,0 +1,27 @@
+package superapp.logic;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException{
+
+    private static final long serialVersionUID = -5869523731922512036L;
+
+    public UnauthorizedException(){}
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(Throwable cause) {
+        super(cause);
+    }
+
+    public UnauthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
